95f5b354584922e70de6d57a180946efc8263aa0
Ben Foxall <benfoxall@gmail.com>
2013-06-09 18:37:10 +0100

    display plugins too, added bootstrapish first demo page

--- public/site.js
+++ public/site.js
@@ -1,42 +1,72 @@
 
+// clear it out on load
+localStorage.setItem('deps', '{}');
+
 var s = document.createElement('span');
 function safe(text){
 	s.innerText = text;
 	return s.innerHTML;
 }
 
+function to_ul(obj){
+	var lis = [];
+	for(var k in obj){
+		if(obj.hasOwnProperty(k)){
+			var used = obj[k];
+
+			// hacky 'z' class to make the used appear on top
+			var classname = used ? 'used':'z';
+
+			lis.push('<li class="'+classname+'"><strong>'+safe(k)+'</strong> - '
+					+ (used ? 'used':'not used')+'</li>');
+
+		}
+	}
+
+	lis.sort();
+	return "<ul>" + lis.join('') + "</ul>";
+}
+
 window.addEventListener("storage", function(e){
 	if(e.key == 'deps'){
-		results.className ='';
-
-		instructions.style.height = instructions.offsetHeight + 'px';
-		instructions.className = 'inactive';
-		setTimeout(function(){
-			instructions.style.height = '0px'	
-		},100)
-		
 
 		var deps = JSON.parse(e.newValue);
 
 
 		// hacky html building,  could use something a lot better in the future
-		var htm = '';
-		var lis = [];
+		var htm = '<h2>Results</h2>';
 
-		var modules = deps.modules || {};
-		for(var k in modules){
-			if(modules.hasOwnProperty(k)){
-				var used = modules[k];
+		htm += '<h3>Core jQuery</h3>';
+		htm += '<h4>More information can be found on the <a href="https://github.com/jquery/jquery#modules">jquery readme</a>. Check out <a href="http://projects.jga.me/jquery-builder/">jquery-builder</a> for an easy way to get a custom build.</h4>'
+		htm += to_ul(deps.modules || {});
 
-				// hacky 'z' class to make the used appear on top
-				var classname = used ? 'used':'z';
 
-				lis.push('<li class="'+classname+'"><strong>'+safe(k)+'</strong> - '
-						+ (used ? 'used':'not used')+'</li>');
+		htm += '<h3>Plugin functions</h3>';
+		htm += '<h4>These are any plugins that have been added to the $.fn namespace.</h4>'
+		htm += to_ul(deps.plugins || {});
 
-			}
-		}
-		lis.sort();
-		results_list.innerHTML = lis.join('');
+
+
+		results.innerHTML = htm;
+
+		// toggle some visibility
+		results.className ='';
+		instructions.className = 'inactive';
 	}
 }, false);
+
+
+
+
+
+
+// hello!
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-35774866-2']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
